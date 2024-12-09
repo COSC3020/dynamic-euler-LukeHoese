b3494dbe0dc4a6512d1b7776ea1885b0b93afb74
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-12-09 14:13:03 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,3 +3,12 @@ const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
 assert(e(10) === 2.7182818011463845);
+assert(factorial(10) === 3628800);
+
+assert(e(5) >= 2.716 - .1 && e(5) <= 2.716 + .1);
+assert(e(100) >= 2.6881 - .1 && e(100) <= 2.6881 + .1);
+
+assert(e(10000) > 2.7182818284 - 0.00001 && e(10000) < 2.7182818284 + 0.00001);
+assert(e(0) === 1);
+assert(e(1) === 2);
+assert(e(-1) === 1);
