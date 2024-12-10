18a11d7792b09630679a26cbb4f276a236684eaf
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-12-10 10:55:31 -0700

    Update code.js

--- code.js
+++ code.js
@@ -6,14 +6,14 @@ function factorial(n, fac) {
 
 function e(n) {
     let result = 1.0;
-    // let fac = 1;
+    let fac = 1;
 
     // implement dynamic programming here. Instead of calculating factorial from scratch at every iteration, we will pass previous factorial as an argument to our factorial function so we do not repeat calculations.
     // previous factorial already calculated as (n-1)! * n, so we save work by passing our already calculated (n-1)! and our next n to the factorial function.
     for (let i = 1; i <= n; i++) {
         // fac = factorial(i, fac)
-        // fac *= i;
-        result += (result * 1/i);
+        fac *= i;
+        result += 1/fac;
     }
 
     return result;
